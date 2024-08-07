aebff5079377148bcd0ab6079eb6ed3114b49c20
indrajeetyadav <indrajeetyadav993648@gmail.com>
2024-08-07 17:13:00 +0530

    Added Feature

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -26,9 +26,10 @@ const LeftBanner = () => {
           />
         </h2>
         <p className="text-base font-bodyFont leading-6 tracking-wide">
-          I use animation as a third dimension by which to simplify experiences
-          and kuiding thro each and every interaction. I'm not adding motion
-          just to spruce things up, but doing it in ways that.
+        I am a passionate MERN developer with good problem-solving skills, 
+          I am dedicated to creating seamless user experiences. I employ animation as a third dimension 
+          to simplify interactions and guide users through every interaction. My goal is to add motion 
+          in meaningful ways, enhancing the overall experience.
         </p>
       </div>
      {/* Media */}
